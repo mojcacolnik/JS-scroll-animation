2c41f9b36f138f9a98c9c598e0051c61c3f05c66
Mojca Colnik <mojca.c13@gmail.com>
2021-10-26 20:28:44 +0200

    feat(): add scroll behaviour

---
+++ script.js
@@ -0,0 +1,19 @@
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+  const triggerBox = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBox) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
